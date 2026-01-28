63690c46ab92041ea6256dec6a763bb0e61c882f
Nacho Ruiz <n.ruizgarcia@hotmail.com>
2026-01-29 00:35:18 +0100

    feat-back: can acces with bearer token to private paths in swagger

--- booqo-api/src/main/java/com/booqo/booqo_api/config/ApplicationConfig.java
+++ booqo-api/src/main/java/com/booqo/booqo_api/config/ApplicationConfig.java
@@ -1,6 +1,10 @@
 package com.booqo.booqo_api.config;
 
 import com.booqo.booqo_api.user.UserRepository;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -43,5 +47,23 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Configuration
+    public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+            final String securitySchemeName = "bearerAuth";
+            return new OpenAPI()
+                    .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                    .components(new Components()
+                            .addSecuritySchemes(securitySchemeName,
+                                    new SecurityScheme()
+                                            .name(securitySchemeName)
+                                            .type(SecurityScheme.Type.HTTP)
+                                            .scheme("bearer")
+                                            .bearerFormat("JWT")));
+        }
     }
 }
