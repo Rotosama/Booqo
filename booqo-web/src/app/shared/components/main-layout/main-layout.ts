e913e5ae31930bd26eba625569aa2fbbfc77392c
Nacho Ruiz <n.ruizgarcia@hotmail.com>
2026-01-28 15:03:36 +0100

    testing lucide icons

--- booqo-web/src/app/shared/components/main-layout/main-layout.ts
+++ booqo-web/src/app/shared/components/main-layout/main-layout.ts
@@ -1,14 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
+import {
+  LucideAngularModule,
+  UserIcon,
+  CalendarIcon,
+  FileIcon,
+  BriefcaseIcon,
+  ChartAreaIcon,
+  SettingsIcon,
+  LogOut,
+} from 'lucide-angular';
+
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, LucideAngularModule],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
 export class MainLayout {
+  userIcon = UserIcon;
+  calendarIcon = CalendarIcon;
+  fileIcon = FileIcon;
+  briefcaseIcon = BriefcaseIcon;
+  barChartIcon = ChartAreaIcon;
+  settingsIcon = SettingsIcon;
+  logoutIcon = LogOut;
   router = inject(Router);
   authService = inject(AuthService);
   userEmail = this.authService.userEmail;
